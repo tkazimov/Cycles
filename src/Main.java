79e50d443e9f2646a6c6ff6ae91efaec6a6ae118
Ilkin <ilkin.mamiyev@gmail.com>
2024-01-17 20:43:51 +0400

    Added Cavid's birthYear

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@ import java.lang.System;
 public class Main {
     public static void main(String[] args) {
         int timaBirthYear = 1993;
-        
+        int cavidBirthYear = 2003;
+
         int firstYear = 1924;
         int lastYear = 2024;
 
 
         boolean yearExists = false;
         for (int year = firstYear; year <= lastYear; year = year + 1) {
-            if (year == timaBirthYear){
+            if (year == timaBirthYear) {
+                yearExists = true;
+
+            }else if(year == cavidBirthYear){
                 yearExists = true;
                 break;
             }
