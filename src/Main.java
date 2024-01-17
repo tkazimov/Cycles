a2b649cf294805ec1eb479dd699d84bc3c8e57f5
Ilkin <ilkin.mamiyev@gmail.com>
2024-01-17 22:34:59 +0400

    added continue operator

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,13 @@ public class Main {
         int lastYear = 2024;
 
         for (int year = firstYear; year <= lastYear; year = year + 1) {
-            if(year % 4 == 0){
-                System.out.println(year);
-                for (int month = 1; month <= 12; month = month + 1){
-                    String zero = month < 10 ? "0" : "";
-                    System.out.println("29." + zero + month + "." + year);
-                }
+            if(year % 4 != 0){
+                continue;
+            }
+            System.out.println(year);
+            for (int month = 1; month <= 12; month = month + 1){
+                String zero = month < 10 ? "0" : "";
+                System.out.println("29." + zero + month + "." + year);
             }
         }
     }
