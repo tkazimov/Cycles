9e3b6756d24202f2c0a60fb87a6eb8be376fb6d3
Teymur Kazimov <teymur1993@gmail.com>
2024-01-17 20:13:31 +0400

    cycle

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.lang.System;
+
+public class Main {
+    public static void main(String[] args) {
+        int timaBirthYear = 1993;
+        
+        int firstYear = 1924;
+        int lastYear = 2024;
+
+
+        boolean yearExists = false;
+        for (int year = firstYear; year <= lastYear; year = year + 1) {
+            if (year == timaBirthYear){
+                yearExists = true;
+                break;
+            }
+        }
+        String found = "nawol";
+        String notfound = "nenawol";
+        System.out.println(yearExists ? found : notfound);
+    }
+}
