5ad18226cc2545b0deb76e101a8c4d8cea2efe0d
Ilkin <ilkin.mamiyev@gmail.com>
2024-01-17 20:57:35 +0400

    Continue operator

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,13 @@ import java.lang.System;
 
 public class Main {
     public static void main(String[] args) {
-        int timaBirthYear = 1993;
-        int cavidBirthYear = 2003;
 
         int firstYear = 1924;
         int lastYear = 2024;
 
-
-        boolean yearExists = false;
         for (int year = firstYear; year <= lastYear; year = year + 1) {
-            if (year == timaBirthYear) {
-                yearExists = true;
-
-            }else if(year == cavidBirthYear){
-                yearExists = true;
-                break;
-            }
+            System.out.println("continue");
         }
-        String found = "nawol";
-        String notfound = "nenawol";
-        System.out.println(yearExists ? found : notfound);
     }
 }
+
